866c8e2ea7757d8a9f4c840f8740b38805c60f59
Brandon Iglesias <bliglesias@live.com>
2026-08-06 12:19:10 -0700

    First FAIL came from forgetting = sign on the terminate condition

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    //  checks for non-number entries
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 
+    if(min < 0 || max < 0) return "ERROR";
+
+    // shorthand "swap"
+    if(min > max) [min, max] = [max, min];
+    /*
+    THE LONG WAY
+    if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+    */
+
+  // Sum the numbers
+  let sum = 0;
+  for(let i = min; i <= max; i++) {sum += i;}
+
+  return sum;
 };
 
 // Do not edit below this line
