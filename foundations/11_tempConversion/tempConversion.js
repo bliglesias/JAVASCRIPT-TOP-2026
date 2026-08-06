9ce64e1f28655572dca6d09dddf8dd6830cee38a
Brandon Iglesias <bliglesias@live.com>
2026-08-06 12:49:02 -0700

    Conversion formulas were correct, but just in the opposite functions

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(int) {
+  // Stole this from Wikipedia
+  let fahrenheit = (int - 32) * (5 / 9);
+
+  // Thanks StackOverflow
+  return Math.round(fahrenheit * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(int) {
+  // Stole this from Wikipedia
+  let celsius = int * (9 / 5) + 32;
+  return Math.round(celsius * 10) / 10;
 };
 
 // Do not edit below this line
